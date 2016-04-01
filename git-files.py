0c96b5a366e51393c9080d508dc95cea4167455d
Dara Javaherian <dara@adverts.ie>
2016-04-01 14:01:07 +0100

    finish off this bad boy

--- git-files.py
+++ git-files.py
@@ -1,6 +1,7 @@
 import sublime, sublime_plugin, os, subprocess
 
 class ExampleCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         sublime.run_command("new_window")
         sublime.active_window().set_layout({
@@ -10,12 +11,37 @@ class ExampleCommand(sublime_plugin.TextCommand):
         })
         settings = sublime.load_settings("Preferences.sublime-settings")
 
-        proc = subprocess.Popen(["cd " + settings.get("git_folder") + "; git diff --name-only"], stdout=subprocess.PIPE, shell=True)
+        proc = subprocess.Popen(["cd " + settings.get("git_folder") + "; git diff master --name-only"], stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
 
         files = out.splitlines();
 
-        for file in files:
-            print (file)
-            sublime.active_window().open_file(settings.get("git_folder") + file.decode("utf-8"));
+        fileExtensions = self.splitUpByFileExtension(files)
+
+        group_num = 0;
+        for k, v in fileExtensions:
+            sublime.active_window().focus_group(group_num)
+            for file in v:
+                sublime.active_window().open_file(settings.get("git_folder") + file);
+            if group_num <= 3:
+                group_num = group_num + 1
         
+    def splitUpByFileExtension(self, files):
+        results = []
+
+        for file in files:
+            filename, file_extension = os.path.splitext(file.decode("utf-8"))
+            print (filename);
+            print (file_extension);
+            found = False;
+            for k, v in results:
+                if file_extension == k:
+                    v.append(file.decode('utf-8'))
+                    found = True;
+
+            if not found:
+                results.append((file_extension, [file.decode("utf-8")]))
+
+        results.sort(key=lambda t: len(t[1]), reverse=True)
+        
+        return results;
